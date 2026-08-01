a17992ac5f19cdf8a7ba4aae03dfc37b6fec0a01
Yakub Hossain <shaikhyakubhossain@gmail.com>
2026-08-01 09:06:48 +0530

    feat: fix import statement for OpenAI and adjust formatting in llm.ts

--- BE/src/config/llm.ts
+++ BE/src/config/llm.ts
@@ -1,13 +1,11 @@
 import { GoogleGenAI } from "@google/genai";
-import OpenAI from "openai";
-
-type Test = typeof OpenAI;
+import { OpenAI } from "openai";
 
 export const gemini = new GoogleGenAI({
-    apiKey: process.env.GEMINI_API_KEY!,
+  apiKey: process.env.GEMINI_API_KEY!,
 });
 
 export const openrouter = new OpenAI({
-    apiKey: process.env.OPENROUTER_API_KEY!,
-    baseURL: "https://openrouter.ai/api/v1",
+  apiKey: process.env.OPENROUTER_API_KEY!,
+  baseURL: "https://openrouter.ai/api/v1",
 });
